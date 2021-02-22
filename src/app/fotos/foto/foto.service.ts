06c9b190dff109786d882143cdaa5b75384af344
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-22 17:55:50 -0300

    Adicionando Modelo de Fotos e tipando retorno de Api

--- src/app/fotos/foto/foto.service.ts
+++ src/app/fotos/foto/foto.service.ts
@@ -1,7 +1,20 @@
+import { Foto } from './foto';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
-
+const Api = 'http://localhost:3000/';
+@Injectable({providedIn: 'root'})
 export class FotoService{
-  constructor(http:HttpClient){}
 
+  constructor(private http:HttpClient){
+
+  }
+  listFromUser(userName:string){
+
+    return this.http
+    .get<Foto[]>(Api+userName+'/photos');
+
+
+  }
 }
+
