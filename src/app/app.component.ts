c38a5ff989b5cf9b4697dfb7d501a07b2d87073a
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-22 17:32:22 -0300

    importando Http Module e adicionando chamada para api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {HttpClient } from '@angular/common/http'
+import { HttpModule } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,14 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  fotos =[
-    {
-      url: 'https://images.unsplash.com/photo-1575239036517-da7514194a05?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
-      description: 'Leão'
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Leoa'
-    }
-  ];
+  fotos: Object[] =[];
+  constructor(http:HttpClient ){
+    http
+    .get<Object[]>('http://localhost:3000/flavio/photos')
+    .subscribe(photos => {
+        this.fotos = photos,
+        err => console.log(err)
+    });
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { FotoModule } from './fotos/fotos.module';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import {  HttpClientModule } from '@angular/common/http';
 import { FotoComponent } from './fotos/foto/foto.component';
 
 
@@ -13,7 +13,8 @@ import { FotoComponent } from './fotos/foto/foto.component';
   ],
   imports: [
     BrowserModule,
-    FotoModule
+    FotoModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
