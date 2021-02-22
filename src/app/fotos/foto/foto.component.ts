9251335a82002f1d3de3d457b311d05aaea5ee77
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-22 16:09:20 -0300

    componente foto

---
+++ src/app/fotos/foto/foto.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector:'ap-foto',
+  templateUrl:'foto.component.html'
+})
+
+export class FotoComponent{
+
+  title = 'app';
+  @Input() url = '';
+
+}
