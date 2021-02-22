e8702a1735ab8b20c5b34f682657da15a526b966
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-22 16:09:40 -0300

    up date app module fotos array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  url = 'https://images.unsplash.com/photo-1613929906219-a2173ecec511?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80';
+
+  fotos =[
+    {
+      url: 'https://images.unsplash.com/photo-1575239036517-da7514194a05?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80',
+      description: 'Leão'
+    },
+    {
+      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
+      description: 'Leoa'
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
+import { FotoModule } from './fotos/fotos.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FotoComponent } from './fotos/foto/foto.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FotoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
