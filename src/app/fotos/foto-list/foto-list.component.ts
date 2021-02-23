2082265473ab82ef21f7b1c50b3c06bb1f132df6
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-23 10:24:26 -0300

    comp foto list

---
+++ src/app/fotos/foto-list/foto-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FotoService } from '../foto/foto.service';
+
+@Component({
+  selector: 'app-foto-list',
+  templateUrl: './foto-list.component.html',
+  styleUrls: ['./foto-list.component.css']
+})
+export class FotoListComponent implements OnInit {
+
+  fotos: Object[] = [];
+  constructor(
+    private fotoService: FotoService,
+    private activatedRoute: ActivatedRoute
+    ) {}
+
+  ngOnInit(): void {
+
+    const username = this.activatedRoute.snapshot.params.userName;
+    this.fotoService
+      .listFromUser(username)
+      .subscribe((photos) => (this.fotos = photos));
+  }
+
+}
--- src/app/fotos/fotos.module.ts
+++ src/app/fotos/fotos.module.ts
@@ -1,10 +1,16 @@
 import { FotoComponent } from './foto/foto.component';
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
+import { FotoListComponent } from './foto-list/foto-list.component';
+import { CommonModule } from '@angular/common';
+import { FotoFormComponent } from './foto-form/foto-form.component';
+import { FotosComponent } from './foto-list/fotos/fotos.component';
 
 @NgModule({
-  declarations:[FotoComponent],
-  exports:[
-    FotoComponent
-  ]
+  declarations:[FotoComponent, FotoListComponent, FotoFormComponent, FotosComponent],
+  imports: [
+    HttpClientModule,
+    CommonModule
+  ],
 })
 export class FotoModule{}
