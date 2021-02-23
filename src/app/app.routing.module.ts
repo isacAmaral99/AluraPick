0faeb2f5af1397e984dc73beff2c50748a845aa5
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-23 10:20:45 -0300

    criando arquivo de rotas

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { FotoFormComponent } from './fotos/foto-form/foto-form.component';
+import { FotoListComponent } from './fotos/foto-list/foto-list.component';
+
+
+
+
+const  routes: Routes=[
+  {path :'user/:userName' , component: FotoListComponent},
+  {path :'user/flavio' , component: FotoFormComponent}
+];
+
+@NgModule({
+ imports:[
+   RouterModule.forRoot(routes)
+ ], exports:[
+   RouterModule
+ ]
+})
+
+export class AppRoutingModule{}
