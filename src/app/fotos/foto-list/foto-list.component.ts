dbd6bb52b339b56b5fe26242421aee0fa76f212b
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-24 09:51:11 -0300

    elementos de foto-list

--- src/app/fotos/foto-list/foto-list.component.ts
+++ src/app/fotos/foto-list/foto-list.component.ts
@@ -1,27 +1,46 @@
+import { FotoService } from './../foto/foto.service';
 import { Foto } from './../foto/foto';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FotoService } from '../foto/foto.service';
+import{debounceTime} from 'rxjs/operators'
+import { from, Subject } from 'rxjs';
+
 
 @Component({
   selector: 'app-foto-list',
   templateUrl: './foto-list.component.html',
   styleUrls: ['./foto-list.component.css']
 })
-export class FotoListComponent implements OnInit {
+export class FotoListComponent implements OnInit , OnDestroy{
 
   fotos: Foto[] = [];
+  filter :string='';
+  debounce :Subject<string> = new Subject<string>();
+  hasMore:boolean = true;
+  currentPage:number=1;
+  userName:string='';
   constructor(
-    private fotoService: FotoService,
-    private activatedRoute: ActivatedRoute
+
+    private activatedRoute: ActivatedRoute,
+    private FotoService:FotoService
+
     ) {}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.fotos = this.activatedRoute.snapshot.data['fotos'];
 
-    const username = this.activatedRoute.snapshot.params.userName;
-    this.fotoService
-      .listFromUser(username)
-      .subscribe((photos) => (this.fotos = photos));
+
+    this.debounce.pipe(debounceTime(300)).subscribe(filter => this.filter = filter)
+
   }
-
+  ngOnDestroy():void{
+    this.debounce.unsubscribe();
+  }
+  load(){
+    this.FotoService.listFromUserPaginated(this.userName,++this.currentPage).subscribe( fotos=> {
+      this.fotos = this.fotos.concat(fotos);
+      if(!fotos.length) this.hasMore= false;
+    });
+  }
 }
