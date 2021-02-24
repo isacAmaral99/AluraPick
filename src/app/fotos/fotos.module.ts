3f5ccca7c8fb70380bf63a75cb38b7a2f0ff15aa
Isac Bueno <isac.bueno@aluno.unip.br>
2021-02-24 09:49:32 -0300

    adicionando filtro e comp de botao

--- src/app/fotos/fotos.module.ts
+++ src/app/fotos/fotos.module.ts
@@ -5,12 +5,14 @@ import { FotoListComponent } from './foto-list/foto-list.component';
 import { CommonModule } from '@angular/common';
 import { FotoFormComponent } from './foto-form/foto-form.component';
 import { FotosComponent } from './foto-list/fotos/fotos.component';
+import { FilterByDescription } from './foto-list/filter-by-description.pipe';
+import { LoadButtonComponent } from './foto-list/load-button/load-button.component';
 
 @NgModule({
-  declarations:[FotoComponent, FotoListComponent, FotoFormComponent, FotosComponent],
+  declarations:[FotoComponent, FotoListComponent, FotoFormComponent, FotosComponent,FilterByDescription,LoadButtonComponent],
   imports: [
     HttpClientModule,
     CommonModule
   ],
 })
-export class FotoModule{}
+export class FotosModule{}
